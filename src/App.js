3a2b2fb927a006906f05c4653794611357ca7a7a
Franco Poffo <franco_sutter@yahoo.com.br>
2023-01-06 12:08:54 -0300

    Criando novos slices no redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Counter from "./components/Counter";
+import { Fragment } from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <Counter />;
+  const loggedIn = useSelector((state) => state.auth.isAuth);
+
+  return (
+    <Fragment>
+      <Header />
+      {!loggedIn ? <Auth /> : <UserProfile />}
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AuthActions } from "../store";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const loggedIn = useSelector((state) => state.auth.isAuth);
+
+  const onLogoutHandler = () => {
+    dispatch(AuthActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {loggedIn && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={onLogoutHandler}>Logout</button>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
